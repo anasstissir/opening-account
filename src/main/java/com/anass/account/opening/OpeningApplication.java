7ec5e8e8cafff9cbf3fffa3f5fba1b92f7176932
Anass Tissir Allah <anasstissirallah@gmail.com>
2023-03-10 19:04:55 +0100

    first commit

---
+++ src/main/java/com/anass/account/opening/OpeningApplication.java
@@ -0,0 +1,13 @@
+package com.anass.account.opening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpeningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anass/account/opening/OpeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anass.account.opening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpeningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
